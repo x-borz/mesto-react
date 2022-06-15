554bb8ff0b3d8b0561f81a85811a81e8e0ec26a6
x-borz <x-borz@yandex.ru>
2022-06-15 15:17:13 +0300

    refactor: отрефакторить открытие попапов на декларативный подход

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,5 +1,4 @@
 function PopupWithForm(props) {
-  console.log(props.children);
   return (
     <section className={`popup popup_type_${props.name} ${props.isOpen && 'popup_opened'}`}>
       <div className="popup__container popup__container_content_common">
