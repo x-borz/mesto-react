f61def4e36dbafbe48a9b77f81a833844c98e668
x-borz <x-borz@yandex.ru>
2022-06-28 12:36:11 +0300

    refactor: клик на оверлей, клик на крестик - закрыть попап

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,13 +1,8 @@
 function ImagePopup({card, onClose}) {
-  const handlePopupMouseDown = evt => {
-    if (evt.target.classList.contains('popup') || evt.target.classList.contains('popup__close-button'))
-      onClose();
-  }
-
   return (
-    <section className={`popup popup_type_image ${card? 'popup_opened' : ''}`} onMouseDown={handlePopupMouseDown}>
-      <div className="popup__container popup__container_content_image">
-        <button className="popup__close-button" type="button"></button>
+    <section className={`popup popup_type_image ${card? 'popup_opened' : ''}`} onMouseDown={onClose}>
+      <div className="popup__container popup__container_content_image" onMouseDown={evt => evt.stopPropagation()}>
+        <button className="popup__close-button" type="button" onMouseDown={onClose}></button>
         <figure className="popup__figure">
           <img className="popup__img" src={card? card.link : '#'} alt={card? card.name : ''}/>
           <figcaption className="popup__caption">{card? card.name : ''}</figcaption>
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,13 +1,8 @@
 function PopupWithForm({name, title, isOpen, isLoading, buttonName, onClose, onSubmit, children}) {
-  const handlePopupMouseDown = evt => {
-    if (evt.target.classList.contains('popup') || evt.target.classList.contains('popup__close-button'))
-      onClose();
-  }
-
   return (
-    <section className={`popup popup_type_${name} ${isOpen? 'popup_opened' : ''}`} onMouseDown={handlePopupMouseDown}>
-      <div className="popup__container popup__container_content_common">
-        <button className="popup__close-button" type="button"></button>
+    <section className={`popup popup_type_${name} ${isOpen? 'popup_opened' : ''}`} onMouseDown={onClose}>
+      <div className="popup__container popup__container_content_common" onMouseDown={evt => evt.stopPropagation()}>
+        <button className="popup__close-button" type="button" onMouseDown={onClose}></button>
         <h2 className={`popup__heading ${name === 'confirmation'? 'popup__heading_type_confirmation' : ''}`}>{title}</h2>
         <form className="popup__form popup__form_type_profile" name={name} onSubmit={onSubmit} noValidate>
           {children}
